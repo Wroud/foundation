c834bda8e2993bd121f83a559275d5334ff7a366
Aleksei Potsetsuev <wrouds@gmail.com>
2024-06-02 18:32:58 +0800

    fix: collect coverage

--- jest.config.js
+++ jest.config.js
@@ -17,6 +17,9 @@ const config = {
   // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
 
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: undefined,
 

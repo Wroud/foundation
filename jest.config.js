0030b4ff5264dafc4e56d77c81853becd4826287
Aleksei Potsetsuev <wrouds@gmail.com>
2024-06-04 18:05:17 +0800

    tests: add json-summary reporter

--- jest.config.js
+++ jest.config.js
@@ -35,12 +35,7 @@ const config = {
   coverageProvider: "v8",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: ["json", "json-summary"],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,

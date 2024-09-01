0a79da60a1eeee693001e9491f88fbeff0c598e6
Aleksei Potsetsuev <wrouds@gmail.com>
2024-09-01 13:55:59 +0800

    chore: Update @wroud/di-react package version to 0.0.2 and add React bindings for DI

---
+++ packages/di-react/src/index.ts
@@ -0,0 +1,7 @@
+export * from "./ServiceProvider.js";
+export * from "./useService.js";
+export * from "./useServiceAsync.js";
+export * from "./useServiceCreateAsyncScope.js";
+export * from "./useServiceCreateScope.js";
+export * from "./useServices.js";
+export * from "./useServicesAsync.js";

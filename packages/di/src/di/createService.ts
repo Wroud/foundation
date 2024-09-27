91765ea9cbfc1ad5506758396b0d0f02d4bc4331
Aleksei Potsetsuev <wrouds@gmail.com>
2024-09-27 23:46:53 +0800

    fix(di): createService result type

--- packages/di/src/di/createService.ts
+++ packages/di/src/di/createService.ts
@@ -1,10 +1,8 @@
-import type { IServiceConstructor } from "../types/index.js";
-
-export function createService<T>(name: string): IServiceConstructor<T> {
+export function createService<T>(name: string): new () => T {
   const obj = {
     [name]() {
       throw new Error(`Service type ${name} can't be initiated`);
     },
   };
-  return obj[name] as unknown as IServiceConstructor<T>;
+  return obj[name] as unknown as new () => T;
 }

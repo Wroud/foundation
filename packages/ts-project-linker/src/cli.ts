3169a9d2af39543082e033288ad595476b487d6c
Aleksei Potsetsuev <wrouds@gmail.com>
2024-10-01 02:13:48 +0800

    feat: add --immutable flag

--- packages/ts-project-linker/src/cli.ts
+++ packages/ts-project-linker/src/cli.ts
@@ -5,19 +5,28 @@ import { link } from "./link.js";
 import glob from "fast-glob";
 
 await yargs(hideBin(process.argv))
+  .scriptName("ts-link")
   .command(
     "glob [patterns..]",
     "Provide a single glob pattern for tsconfig.json locations",
     (yargs) => {
-      return yargs.positional("patterns", {
-        description: "Glob patterns to match tsconfig.json locations",
-        type: "string",
-        array: true,
-        demandOption: true,
-      });
+      return yargs
+        .positional("patterns", {
+          description: "Glob patterns to match tsconfig.json locations",
+          type: "string",
+          array: true,
+          demandOption: true,
+        })
+        .options({
+          immutable: {
+            type: "boolean",
+            description:
+              "Do not modify the tsconfig.json files, will exit with 1 if changes are needed",
+          },
+        });
     },
     async (argv) => {
-      const { patterns } = argv;
+      const { patterns, immutable } = argv;
       if (!patterns) {
         console.error("Error: No glob pattern provided");
         process.exit(1);
@@ -29,25 +38,33 @@ await yargs(hideBin(process.argv))
         console.error("Error: No paths found");
         process.exit(1);
       }
-      await link({}, ...paths);
+      await link({ immutable }, ...paths);
     },
   )
   .command(
     ["link [paths..]", "* [paths..]"],
     "link ts project references",
     (yargs) => {
-      return yargs.positional("paths", {
-        description: "Paths to tsconfig.json locations",
-        type: "string",
-        array: true,
-      });
+      return yargs
+        .positional("paths", {
+          description: "Paths to tsconfig.json locations",
+          type: "string",
+          array: true,
+        })
+        .options({
+          immutable: {
+            type: "boolean",
+            description:
+              "Do not modify the tsconfig.json files, will exit with 1 if changes are needed",
+          },
+        });
     },
     async (argv) => {
       if (!argv.paths || argv.paths.length === 0) {
         console.error("Error: No paths provided");
         process.exit(1);
       }
-      await link({}, ...argv.paths);
+      await link({ immutable: argv.immutable }, ...argv.paths);
     },
   )
   .strict()

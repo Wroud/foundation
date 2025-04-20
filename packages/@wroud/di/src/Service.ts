816ce5d4fbdf976059095c7a4c9d72c34bcf173d
Wroud <wrouds@gmail.com>
2025-04-20 13:42:18 +0800

    chore: improve types

--- packages/@wroud/di/src/Service.ts
+++ packages/@wroud/di/src/Service.ts
@@ -1,11 +1,11 @@
-export class Service {
+export class Service implements Disposable, AsyncDisposable {
   protected dispose(): Promise<void> | void {}
 
   [Symbol.dispose]() {
     return this.dispose();
   }
 
-  [Symbol.asyncDispose]() {
-    return this.dispose();
+  async [Symbol.asyncDispose]() {
+    return await this.dispose();
   }
 }

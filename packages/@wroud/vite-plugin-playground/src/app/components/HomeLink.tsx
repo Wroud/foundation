282f6c5306904a79a5f6dc569d77ef71831e9d51
Wroud <wrouds@gmail.com>
2025-04-22 17:14:20 +0800

    fix: links

--- packages/@wroud/vite-plugin-playground/src/app/components/HomeLink.tsx
+++ packages/@wroud/vite-plugin-playground/src/app/components/HomeLink.tsx
@@ -1,13 +1,28 @@
 import { useBase } from "@wroud/vite-plugin-ssg/react/components";
+import { useNavigation } from "@wroud/playground-react/views";
+import { PlaygroundRoutes } from "@wroud/playground";
 import logo from "./logo.svg?no-inline";
 
 export function HomeLink() {
   const base = useBase();
+  const navigation = useNavigation();
   return (
     <a
       aria-label="Home"
-      href="/"
       className="twp:flex twp:items-center twp:gap-2"
+      href={
+        navigation.router.matcher?.stateToUrl({
+          id: PlaygroundRoutes.root,
+          params: {},
+        }) ?? "#"
+      }
+      onClick={(e) => {
+        e.preventDefault();
+        navigation.navigate({
+          id: PlaygroundRoutes.root,
+          params: {},
+        });
+      }}
     >
       <img src={base(logo)} alt="Playground" className="twp:w-6 twp:h-6" />
       <div className="twp:text-sm twp:font-bold">Playground</div>
--- packages/@wroud/vite-plugin-playground/src/app/pages/Template.tsx
+++ packages/@wroud/vite-plugin-playground/src/app/pages/Template.tsx
@@ -1,4 +1,8 @@
+import { useNavigation } from "@wroud/playground-react/views";
+import { PlaygroundRoutes } from "@wroud/playground";
+
 export function Template() {
+  const navigation = useNavigation();
   return (
     <main className="twp:flex twp:h-full twp:flex-col twp:p-4 twp:m:p-8 twp:lg:p-16 twp:overflow-auto twp:flex-auto">
       <article className="twp:flex twp:flex-col">
@@ -12,8 +16,20 @@ export function Template() {
           </p>
           <div className="twp:not-prose twp:mt-6 twp:mb-16 twp:flex twp:gap-3">
             <a
-              className="twp:inline-flex twp:gap-0.5 twp:justify-center twp:overflow-hidden twp:text-sm twp:font-medium twp:transition twp:rounded-full twp:bg-zinc-900 twp:py-1 twp:px-3 twp:text-white twp:hover:bg-zinc-700 twp:dark:bg-emerald-400/10 twp:dark:text-emerald-400 twp:dark:ring-1 twp:dark:ring-inset twp:dark:ring-emerald-400/20 twp:dark:hover:bg-emerald-400/10 twp:dark:hover:text-emerald-300 twp:dark:hover:ring-emerald-300"
-              href="/components"
+              className="twp:inline-flex twp:gap-0.5 twp:justify-center twp:overflow-hidden twp:text-sm twp:font-medium twp:transition twp:rounded-full twp:bg-zinc-900 twp:py-1 twp:px-3 twp:text-white twp:hover:bg-zinc-700 twp:dark:bg-emerald-400/10 twp:dark:text-emerald-400 twp:dark:ring-1 twp:dark:ring-inset twp:dark:ring-emerald-400/20 twp:dark:hover:bg-emerald-400/10 twp:dark:hover:text-emerald-300 twp:dark:hover:ring-emerald-300 twp:no-underline"
+              href={
+                navigation.router.matcher?.stateToUrl({
+                  id: PlaygroundRoutes.components,
+                  params: {},
+                }) ?? "#"
+              }
+              onClick={(e) => {
+                e.preventDefault();
+                navigation.navigate({
+                  id: PlaygroundRoutes.components,
+                  params: {},
+                });
+              }}
             >
               Browse Components
               <svg
@@ -31,8 +47,9 @@ export function Template() {
               </svg>
             </a>
             <a
-              className="twp:inline-flex twp:gap-0.5 twp:justify-center twp:overflow-hidden twp:text-sm twp:font-medium twp:transition twp:rounded-full twp:py-1 twp:px-3 twp:text-zinc-700 twp:ring-1 twp:ring-inset twp:ring-zinc-900/10 twp:hover:bg-zinc-900/2.5 twp:hover:text-zinc-900 twp:dark:text-zinc-400 twp:dark:ring-white/10 twp:dark:hover:bg-white/5 twp:dark:hover:text-white"
+              className="twp:inline-flex twp:gap-0.5 twp:justify-center twp:overflow-hidden twp:text-sm twp:font-medium twp:transition twp:rounded-full twp:py-1 twp:px-3 twp:text-zinc-700 twp:ring-1 twp:ring-inset twp:ring-zinc-900/10 twp:hover:bg-zinc-900/2.5 twp:hover:text-zinc-900 twp:dark:text-zinc-400 twp:dark:ring-white/10 twp:dark:hover:bg-white/5 twp:dark:hover:text-white twp:no-underline"
               href="/guide"
+              hidden
             >
               Usage Guide
             </a>

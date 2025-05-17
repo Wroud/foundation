4bab8f29c512eb6a3542170e5eebc406ed3d4956
Alexey <wrouds@gmail.com>
2025-05-18 01:57:37 +0800

    refactor(vite-plugin-ssg): rename html tag utilities (#25)

--- packages/@wroud/vite-plugin-ssg/src/utils/ssgHtmlTags.ts
+++ packages/@wroud/vite-plugin-ssg/src/utils/ssgHtmlTags.ts
@@ -1,10 +1,10 @@
 import { trailingSeparatorRE } from "./trailingSeparatorRE.js";
 
 export const ssgRE = /(\?|&)ssg-html-tags(?:&|$)/;
-export function removeSSgHtmlTagsQuery(url: string): string {
+export function removeSsgHtmlTagsQuery(url: string): string {
   return url.replace(ssgRE, "$1").replace(trailingSeparatorRE, "");
 }
 
-export function isSSgHtmlTagsId(id: string) {
+export function isSsgHtmlTagsId(id: string) {
   return ssgRE.test(id);
 }

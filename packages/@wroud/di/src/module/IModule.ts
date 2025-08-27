c3b1906147413e5e5d5ca65489fbf04fcc2b0235
Wroud <wrouds@gmail.com>
2025-08-27 14:33:38 +0800

    fix: allow synchronous configure function

--- packages/@wroud/di/src/module/IModule.ts
+++ packages/@wroud/di/src/module/IModule.ts
@@ -2,5 +2,5 @@ import type { IServiceCollection } from "../types/index.js";
 
 export interface IModule {
   name: string;
-  configure(serviceCollection: IServiceCollection): Promise<void>;
+  configure(serviceCollection: IServiceCollection): Promise<void> | void;
 }
